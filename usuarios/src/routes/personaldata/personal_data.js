7ae4384b0ecfdbbd2264c54ded571f0dbf936c5f
Kaio Eduardo <kaiocarvalhode@gmail.com>
2024-06-20 15:04:25 -0300

    Feature: Finalizaçao do conteudo da rota personal_data

--- usuarios/src/routes/personaldata/personal_data.js
+++ usuarios/src/routes/personaldata/personal_data.js
@@ -1,11 +1,12 @@
-const { config } = require("dotenv")
+ require ("dotenv").config()
 const express = require("express")
-const router_personal = express.Router
+const router_personal = express.Router()
 const data = require("../../database/config.js")
 const router = require("../users/users")
+const verifica = require("../../middleware/verify_token.js")
 
 
-router_personal.length("/listar",(req,res)=>{
+router_personal.get("/listar",verifica, (req,res)=>{
     data.query("select * from dadospessoais",(error,result)=>{
         if (error) {
             return res.status(500).send({msg:"erro ao ler os dados "})
@@ -14,7 +15,7 @@ router_personal.length("/listar",(req,res)=>{
     })
 })
 
-router_personal.get("/listar/:cpf",(req,res)=>{
+router_personal.get("/listar/:cpf",verifica, (req,res)=>{
     data.query("select * from dadospessoais",req.params.cpf,(error,result) => {
         if (error) {
             return res.status(500).send({msg:"erro ao tentar carregar os dados "})
@@ -23,8 +24,8 @@ router_personal.get("/listar/:cpf",(req,res)=>{
 })
 })
 
-router_personal.post("/cadastrar",(req,res)=>{
-    data.query("insert into dadospessoais set?",req,body,(erro,result)=>{
+router_personal.post("/cadastrar",verifica, (req,res)=>{
+    data.query("insert into dadospessoais set?" ,req.body,(erro,result)=>{
         if (erro) {
             return res.status(500).send({msg:"erro ao tentar cadastro"})
 
@@ -32,3 +33,14 @@ router_personal.post("/cadastrar",(req,res)=>{
         res.status(201).send({msg:"ok",playload:result})
     })
 })
+
+router_personal.put("/atualizar/id:",verifica, (req,res)=>{
+    data.query("update dadospessoais set ? where iddadospessoais=?",[req.body,req.params.id],(error,result)=>{
+        if (error) {
+            return res.status(500).send({msg:"erro ao tentar atualizar os dados "})
+        }
+        res.status(200).send({msg:"ok",payload:result})
+    })
+})
+
+module.exports = router_personal
